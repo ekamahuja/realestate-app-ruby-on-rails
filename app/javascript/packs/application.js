072b92999982b25e440020284fc39b9d34207974
Ekam <ekamahuju@gmail.com>
2021-11-19 17:36:02 +0000

    Working on importing theme to the site using asset pipline

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,48 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// Theme Custom JavaScript
+// require("./theme/theme/ace-responsive-menu")
+// require("./theme/theme/jquery.mmenu.all")
+// require("./theme/bootstrap-select.min")
+// require("./theme/bootstrap.min")
+// require("./theme/chart-custome")
+// require("./theme/chart.min")
+// require("./theme/dashboard-script")
+// require("./theme/google-map-api")
+// require("./theme/googlemaps1")
+// require("./theme/googlemaps2")
+// require("./theme/infobox.min")
+// require("./theme/isotop")
+// require("./theme/jquery-3.6.0")
+// require("./theme/jquery-migrate-3.0.0.min")
+// require("./theme/jquery-scrolltofixed-min")
+// require("./theme/jquery.counterup")
+// require("./theme/jquery.smartuploader")
+// require("./theme/maps")
+// require("./theme/markerclusterer")
+// require("./theme/parallax")
+// require("./theme/popper.min")
+// require("./theme/pricing-slider")
+// require("./theme/progressbar")
+// require("./theme/script")
+// require("./theme/scrollto")
+// require("./theme/simplebar")
+// require("./theme/single-counter")
+// require("./theme/slider")
+// require("./theme/smartuploader")
+// require("./theme/snackbar.min")
+// require("./theme/tagsinput")
+// require("./theme/timepicker")
+// require("./theme/wow.min")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
+
+
+
+
+// Uncomment to copy all static images under ../theme/images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+// const images = require.context('../theme/images', true)
 // const imagePath = (name) => images(name, true)
