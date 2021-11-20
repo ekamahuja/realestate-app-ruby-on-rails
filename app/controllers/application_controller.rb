cbceea9d4d03cc8b2814b32ae0100fedfa16645e
Ekam <ekamahuju@gmail.com>
2021-11-20 03:09:54 +0000

    Fixed bug with devise (where first_name and last_name wont store) and removed type, and worked on public main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_premitted_parameters, if: :devise_controller?
+
+    protected
+    def configure_premitted_parameters
+        devise_parameter_sanitizer.permit(:udpate, keys: [:first_name, :last_name, :url])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
 end
--- db/migrate/20211119142746_devise_create_accounts.rb
+++ db/migrate/20211119142746_devise_create_accounts.rb
@@ -8,7 +8,6 @@ class DeviseCreateAccounts < ActiveRecord::Migration[6.0]
       t.string :last_name,          null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :type,               null: false, default: ""
       t.string :url
 
       ## Recoverable
