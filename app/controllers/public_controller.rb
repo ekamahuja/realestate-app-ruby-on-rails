f193b411c6673f1c6a05897f4d9d66c52644b8b9
Ekam <ekamahuju@gmail.com>
2021-11-20 10:56:54 +0000

    When user is signed in root directory now redirects to dashboard automatically

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,8 @@
 class PublicController < ApplicationController
   def main
+    if account_signed_in? 
+      redirect_to dashboard_path, flash: {success: "Successfully Signed In!"} and return
+    end
     @properties = Property.latest
   end
 end
