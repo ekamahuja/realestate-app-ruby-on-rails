bcdaa05bdcfe3bc97f495c9b899c10a616167a8b
Ekam <ekamahuju@gmail.com>
2021-11-20 05:18:51 +0000

    Added latest properties to public main page

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,15 @@
 module PropertiesHelper
+    def property_thumbnail(property)
+        photo = property.photo.attached? ? property.photo : "placeholder.jpg"
+        image_tag photo, class: "bd-placeholder-img card-img-top", width: "100%", height: "225"
+    end
+
+    def property_bathroom(property)
+    bathrooms = property.rooms - property.bathrooms
+        if bathrooms < 0
+            return bathrooms * -1;
+        else 
+            return bathrooms
+        end
+    end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,7 @@ class Property < ApplicationRecord
 
     #Active Storage Realtionship with property
     has_one_attached :photo
+
+    # Gete Latest Properties
+    scope :latest, -> { order created_at: :desc}
 end
