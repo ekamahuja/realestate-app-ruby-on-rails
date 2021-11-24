b3305b2490e595d6b9236d51f6271450bad63d6e
Ekam <ekamahuju@gmail.com>
2021-11-24 07:58:49 +0000

    Fixed issue where a users coudve changed html to send text as any user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
   
     def create
       @message = @conversation.messages.new(message_params)
-
+      @message.account_id = current_account.id
       if @message.save
         redirect_to conversation_messages_path(@conversation)
       end
