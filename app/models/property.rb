8fb064468cfa79b7a6dbf48b54ebe0b9e6ad71e9
Ekam <ekamahuju@gmail.com>
2021-11-24 16:22:57 +0000

    Minior changes to scopes, and how agent information and how email model is displayed

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,10 +7,10 @@ class Property < ApplicationRecord
     # Get Latest Properties
     scope :latest, -> { order created_at: :desc }
 
-    scope :sold, -> { where(for_sale: true, status "sold") }
-    scope :for_sale, -> { where(for_sale: true, status "avaliable") }
-    scope :leased, -> { where(for_sale: false, status "leased") }
-    scope :for_rent, -> { where(for_sale: false, status "avaliable") }
+    scope :sold, -> { where for_sale: true, status: "sold" }
+    scope :for_sale, -> { where for_sale: true, status: "avaliable" }
+    scope :leased, -> { where for_sale: false, status: "leased" }
+    scope :for_rent, -> { where for_sale: false, status: "avaliable" }
 
 
 
