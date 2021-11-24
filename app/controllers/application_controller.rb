d2115fe5b1afa1376d3e59c75cae74595572f897
Ekam <ekamahuju@gmail.com>
2021-11-24 15:55:00 +0000

    Added description to accounts so it can be used on profile#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_premitted_parameters
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :url, :profile_picture, :role, :company_name, :phone_number])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :url, :profile_picture, :role, :company_name, :phone_number, :description])
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role, :phone_number])
     end
 
